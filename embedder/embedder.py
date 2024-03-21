e2e6dd2830301ac7ba55b0b11ec6cf8e9b8768fd
pururoong <pururoong@gmail.com>
2024-03-21 16:12:48 +0900

    vector DB milvus로 변경.

--- embedder/embedder.py
+++ embedder/embedder.py
@@ -1,35 +1,35 @@
-import chromadb
-from langchain.document_loaders import PyPDFLoader
-from langchain.vectorstores import Chroma
+from langchain_community.document_loaders import PyPDFLoader
+from langchain_community.vectorstores import Milvus
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.chains import RetrievalQA, ConversationalRetrievalChain
 from langchain.embeddings import SentenceTransformerEmbeddings
 
+## DB Connect
+
+
+## Text Embed
 loader = PyPDFLoader("./sample.pdf")
 
 text_splitter = RecursiveCharacterTextSplitter(
     chunk_size=30,
-    chunk_overlap=5
+    chunk_overlap=10
     )
 
 pages = loader.load_and_split(text_splitter)
+
+print(pages)
+
 embeddings = SentenceTransformerEmbeddings(model_name="paraphrase-MiniLM-L6-v2")
 
-#client = chromadb.HttpClient(host="localhost", port=8000)
 
-# Create a persistent, file-based vector store, using Chroma vector store.
-directory = 'index_store'
-vector_index = Chroma.from_documents(
-    pages, # Documents
-    embeddings, # Text embedding model
-    persist_directory=directory # persists the vectors to the file system
-    )
+vector_db = Milvus.from_documents(
+    pages,
+    embeddings,
+    connection_args={"host": "127.0.0.1", "port": "19530"},
+)
 
+query = "오늘 날짜는?"
+docs = vector_db.similarity_search(query)
 
-vector_index.persist()
+print (docs)
 
-query = "오늘 기온 온도?"
-matching_docs = vector_index.similarity_search(query)
-
-
-print(matching_docs)
