2eb2725fd7e0f51c6c28944c92106b1bca792813
김준형 <joon1979@ntels.com>
2024-06-13 13:41:27 +0900

    메뉴 구조 개선

--- gui/static/scripts/chat_script.js
+++ gui/static/scripts/chat_script.js
@@ -5,7 +5,7 @@ async function sendQuery() {
     if (query.trim() === '') return;
 
     const chatbox = document.getElementById('chat-box');
-    chatbox.innerHTML += `<div class="message user"><div class="bubble user">${query}</div></div>`;
+    chatbox.innerHTML += `<div class="message user"><div class="bubble user">${convertTextToHtml(query)}</div></div>`;
     chatbox.innerHTML += `<div class="message bot loading"><div class="bubble bot">Loading...</div></div>`;
     chatbox.scrollTop = chatbox.scrollHeight;
 
@@ -32,13 +32,29 @@ async function sendQuery() {
             loadingMessage.remove();
         }
 
-        chatbox.innerHTML += `<div class="message bot"><div class="bubble bot">${answer}</div></div>`;
+        chatbox.innerHTML += `<div class="message bot"><div class="bubble bot">${convertTextToHtml(answer)}</div></div>`;
     } catch (error) {
         console.error('Fetch error:', error);
         chatbox.innerHTML += `<div class="message bot"><div class="bubble bot">Error fetching response</div></div>`;
     }
 
     chatbox.scrollTop = chatbox.scrollHeight;  // Scroll to the bottom
+}
+
+function convertTextToHtml(text) {
+    // HTML 특수 문자 이스케이프
+    let escapedText = text
+        .replace(/&/g, "&amp;")
+        .replace(/</g, "&lt;")
+        .replace(/>/g, "&gt;");
+
+    // 줄바꿈을 <br>로 변환
+    escapedText = escapedText.replace(/\n/g, "<br>");
+
+    // 여러 공백을 &nbsp;로 변환
+    escapedText = escapedText.replace(/ /g, "&nbsp;");
+
+    return escapedText;
 }
 
 $(document).ready(function() {
@@ -52,4 +68,3 @@ $(document).ready(function() {
         }
     });
 });
-
